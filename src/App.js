f8ee89cd57862728127972695989d0439d762399
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2022-10-02 20:00:14 -0400

    :zap: added vanta.js background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 import  Layout  from './Layout';
+import React, { useEffect, useContext, useState, useRef } from 'react'
 
 function App() {
+
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        window.VANTA.HALO({
+          el: "#background-ring",
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: true,
+          baseColor: "cyan",
+          backgroundColor: "#baff8f",
+          amplitudeFactor: 3,
+          xOffset: -0.11,
+          yOffset: 0.06,
+          size: 2.35
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="App">
+    <div>
+      <div id="background-ring" className="fixed opacity-80 -z-40 w-full min-h-screen flex flex-col text-center"></div>
       <Layout />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useContext, useState } from 'react'
+import React, { useEffect, useContext, useState, useRef } from 'react'
 import { Context } from '../store/appContext';
 import Card from './Card.js';
 import FloatingButtom from './FloatingButtom.js';
 import { Modal } from './Modal';
+import logo from '../assets/logo.png'
+
 
 export const Home = () => {
 
@@ -20,8 +22,9 @@ export const Home = () => {
   }
 
   return (
-    <div style={{ background: 'white' }} className="w-full h-full flex flex-col text-center">
-      <h1 className='text-3xl md:text-6xl font-bold my-4 md:my-8'>{store.name} </h1>
+    <div className="w-full h-full flex flex-col text-center">
+      {/* <h1 className='text-3xl md:text-6xl font-bold my-4 md:my-8'>{store.name} </h1> */}
+      <img src={logo} style={{ padding: '3rem', paddingBottom: '0', maxWidth: '700px', margin: 'auto'}} alt="Rick and Morty"/>
       <div className='flex flex-wrap '>
         {store.characters.map(char=> {return <Card key={char.id} character={char} />})}
         <div className='flex flex-row w-full'>
